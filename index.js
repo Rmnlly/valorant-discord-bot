51ee6bc8208e8ce8b687ca706996dbb805ab7220
Raman <33904740+Rmnlly@users.noreply.github.com>
2020-08-16 15:27:50 -0400

    added data retrieval link and some rough reponse logic

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import env from "dotenv";
 env.config();
 import Discord from "discord.js";
-// const Discord = require("discord.js");
+import fetch from "node-fetch";
+
 const client = new Discord.Client();
+
+const blitzPlayerBaseUrl = "https://valorant.iesdev.com/player";
+
+const blitzTierMap = [
+  "tier retrieval error",
+  null,
+  null,
+  "iron 1",
+  "iron 2",
+  "iron 3",
+  "bronze 1",
+  "bronze 2",
+  "bronze 3",
+  "silver 1",
+  "silver 2",
+  "silver 3",
+  "gold 1",
+  "gold 2",
+  "gold 3",
+  "plat 1",
+  "plat 2",
+  "plat 3",
+];
+
+const snarkyRemark = (tier) =>
+  tier >= 15 ? "so, yes, surprisingly" : "so no, unsurprisingly";
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -10,7 +37,16 @@ client.once("ready", () => {
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
+  if (message.content.toLowerCase() === "is raman plat yet") {
+    const data = await fetch(`${blitzPlayerBaseUrl}/koshaku-ont`);
+    const parsedData = await data.json();
+    const blitzTier = parsedData.ranks.competitive.tier || 0;
+    const rank = blitzTierMap[blitzTier];
+    message.channel.send(
+      `Raman is currently ${rank}${snarkyRemark(blitzTier)}`
+    );
+  }
   if (message.content === "lame") {
     message.channel.send("why me?");
   }
